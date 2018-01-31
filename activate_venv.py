365a7ff18bf722fe8bb91e47f4cd85f1fe841509
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-01-31 11:47:22 -0200

    Refactor activate_venv.py

--- activate_venv.py
+++ activate_venv.py
@@ -5,23 +5,27 @@ import sys
 import subprocess
 import venv
 
-assert sys.version_info.major == 3 and sys.version_info.minor >= 6, 'Use python3 >= 3.6'
-assert len(sys.argv) > 1, 'Pass target executable as an argument'
-base_dir = Path(__file__).resolve().parent
-
-venv.create(base_dir / 'venv', with_pip=True)
-
 
 def parse_environment(environment):
     key_values = [x.partition('=') for x in environment.splitlines()]
     return {key: value for key, _, value in key_values}
 
 
-environment = subprocess.check_output([
-    'bash', '-c', 'source {} && env'.format(base_dir / 'venv/bin/activate')
-], universal_newlines=True)
-os.environ.update(parse_environment(environment))
+def main(target_command):
+    base_dir = Path(__file__).resolve().parent
+    venv.create(base_dir / 'venv', with_pip=True)
 
-subprocess.check_call(['pip', 'install', '-r', base_dir / 'requirements.txt'])
+    environment = subprocess.check_output([
+        'bash', '-c', 'source {} && env'.format(base_dir / 'venv/bin/activate')
+    ], universal_newlines=True)
+    os.environ.update(parse_environment(environment))
 
-os.execvp(sys.argv[1], sys.argv[1:])
+    subprocess.check_call(['pip', 'install', '-r', base_dir / 'requirements.txt'])
+
+    os.execvp(target_command[0], target_command)
+
+
+if __name__ == '__main__':
+    assert sys.version_info.major == 3 and sys.version_info.minor >= 6, 'Use python3 >= 3.6'
+    assert len(sys.argv) > 1, 'Pass target command as an argument'
+    main(sys.argv[1:])
