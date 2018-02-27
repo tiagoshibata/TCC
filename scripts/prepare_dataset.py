b96a1947efcece105f4f1abb06f921e529690f46
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-02-26 23:34:22 -0300

    Validate frames at dataset preparation

--- scripts/prepare_dataset.py
+++ scripts/prepare_dataset.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import argparse
 import hashlib
+from itertools import count
 from pathlib import Path
+import sys
+import time
 
-import ffmpy
+import cv2
 
 
 def parse_args():
@@ -11,6 +14,11 @@ def parse_args():
     parser.add_argument('source', help='source video')
     parser.add_argument('destination', help='destination directory')
     return parser.parse_args()
+
+
+def fail(message, *args, **kwargs):
+    print(message, file=sys.stderr, *args, **kwargs)
+    raise SystemExit(1)
 
 
 def hash_file(filename):
@@ -21,15 +29,55 @@ def hash_file(filename):
     return digest.hexdigest()
 
 
+class FrameValidationException(Exception):
+    pass
+
+
+def validate_frame(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    histogram = cv2.calcHist([gray], [0], None, [64], [0, 256]) / (gray.shape[0] * gray.shape[1])
+    if max(histogram) >= 0.96:
+        raise FrameValidationException('Histogram shows few light variation')
+
+
+def is_new_scene(frame, previous):
+    if previous is None:
+        return True
+    # TODO Compute SSIM
+    return False
+
+
 def main(args):
     file_hash = hash_file(args.source)
-    destination = Path(args.destination) / file_hash
-    destination.mkdir()  # throws if already exists
-    output = destination / '%06d.png'  # %06d will be processed by ffmpeg
-    ffmpy.FFmpeg(
-        inputs={args.source: None},
-        outputs={str(output): None},
-    ).run()
+    destination = Path(args.destination)
+    if not destination.exists():
+        fail('Folder {} does not exist'.format(destination))
+    if any(destination.glob('{}_*'.format(file_hash))):
+        fail('Video has already been processed (folders {}/{}_* exist)'.format(destination, file_hash))
+
+    video = cv2.VideoCapture(args.source)
+    frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT)) or '<Unknown frame count>'
+    start_time = time.time()
+    previous = None
+    for i in count():
+        has_frame, frame = video.read()
+        if not has_frame:
+            break
+        try:
+            validate_frame(frame)
+            if is_new_scene(frame, previous):
+                print('Frame {} is a new scene'.format(i))
+                scene_destination = destination / '{}_{:06d}'.format(file_hash, i)
+                scene_destination.mkdir()
+                output = str(scene_destination / '{:06d}.png')
+            cv2.imwrite(output.format(i), frame)
+            previous = frame
+            if not i % 1000:
+                print('Frame {}/{}: {} s'.format(i, frame_count, time.time() - start_time))
+        except FrameValidationException as e:
+            print('Invalid frame {}: {}'.format(i, e))
+            previous = None
+    video.release()
 
 
 if __name__ == '__main__':
