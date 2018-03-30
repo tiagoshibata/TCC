ca373093e9b8e8a1d6d15e5948301c874f1c6847
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-03-30 00:11:51 -0300

    Add Scale layer

--- colormotion/nn/layers.py
+++ colormotion/nn/layers.py
@@ -1,7 +1,7 @@
 from keras.layers import Lambda
 
 
-def Crop(axis, start, end):  # pylint: disable=invalid-name
+def Crop(axis, start, end, **kwargs):  # pylint: disable=invalid-name
     def f(x):
         if axis == 0:
             return x[start:end]
@@ -14,4 +14,8 @@ def Crop(axis, start, end):  # pylint: disable=invalid-name
         if axis == 4:
             return x[:, :, :, :, start:end]
         raise NotImplementedError()
-    return Lambda(f)
+    return Lambda(f, **kwargs)
+
+
+def Scale(amount, **kwargs):  # pylint: disable=invalid-name
+    return Lambda(lambda x: x * amount, **kwargs)
--- tests/colormotion/nn/test_layers.py
+++ tests/colormotion/nn/test_layers.py
@@ -1,8 +1,10 @@
 from keras import backend as backend
 from keras.layers import Input
+from keras.models import Sequential
+import numpy as np
 import pytest
 
-from colormotion.nn.layers import Crop
+from colormotion.nn.layers import Crop, Scale
 
 
 @pytest.mark.parametrize('crop_args,expected_shape', [
@@ -17,6 +19,18 @@ from colormotion.nn.layers import Crop
         (None, 32, 32, 2),
     ),
 ])
-def test_crop(crop_args, expected_shape):
-    x = Input(shape=(32, 32, 4), name="input")
+def test_Crop(crop_args, expected_shape):
+    x = Input(shape=(32, 32, 4))
     assert backend.int_shape(Crop(*crop_args)(x)) == expected_shape
+
+
+def test_Scale():
+    m = Sequential()
+    m.add(Scale(10, input_shape=(2, 2, 3)))
+    data = np.array(np.arange(12).reshape((2, 2, 3)), ndmin=4)
+    assert (m.predict(data, batch_size=1) == np.array([
+        [
+            [[0, 10, 20], [ 30, 40, 50]],
+            [[60, 70, 80], [90, 100, 110]],
+        ]
+    ])).all()
