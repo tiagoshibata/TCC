2338b77db1b2c26752a48c280b25b62eb51e2658
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-04-03 10:59:25 -0300

    Complete model based on interactive deep colorization

--- colormotion/nn/model.py
+++ colormotion/nn/model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-from keras.layers import Activation, Add, BatchNormalization, Conv2D, Conv2DTranspose, Input, Lambda
+from keras.initializers import RandomNormal
+from keras.layers import Activation, Add, AveragePooling2D, BatchNormalization, Conv2D, Conv2DTranspose, Input, Lambda
+from keras.layers.advanced_activations import LeakyReLU
 from keras.models import Model, Sequential
 
 from colormotion.nn.layers import Scale
@@ -13,6 +15,10 @@ def previous_frame_input():
     state_input = Input(shape=(256, 256, 3), name='state_input')  # state from previous frame
     l_input = Input(shape=(256, 256, 1), name='l_input')
 
+    def Conv2D_default(filters, **kwargs):  # pylint: disable=invalid-name
+        '''Conv2D block with most commonly used options.'''
+        return Conv2D(filters, 3, padding='same', activation='relu', **kwargs)
+
     # conv1
     # conv1_1
     state = Conv2D(64, 3, padding='same')(state_input)
@@ -20,135 +26,102 @@ def previous_frame_input():
     x = Add()([state, x])
     x = Activation('relu')(x)
     # conv1_2
-    x = Conv2D(64, 3, padding='same', activation='relu')(x)
+    x = Conv2D_default(64)(x)
     conv1_2norm = BatchNormalization()(x)
-    x = Conv2D(64, 1, strides=2, use_bias=False, kernel_initializer='ones')(conv1_2norm)
-    # FIXME Apply following parameters (from Caffe protobuf description):
-    # layer {
-    #   param {lr_mult: 0 decay_mult: 0} # UNARY_BRANCH_PROPAGATE
-    #   convolution_param {
-    #     group: 64
-    #   }
-    # }
+    x = AveragePooling2D()(x)
+    # TODO Real-Time User-Guided Image Colorization with Learned Deep Priors uses a different method to half the
+    # spatial dimension: it samples each input 2d tensor with stride 2 instead of pooling the average.
 
     # conv2
     # conv2_1
-    x = Conv2D(128, 3, padding='same', activation='relu')(x)
+    x = Conv2D_default(128)(x)
     # conv2_2
-    x = Conv2D(128, 3, padding='same', activation='relu')(x)
-    conv2_2norm = BatchNormalization()(x)  # TODO param {lr_mult: 0 decay_mult: 0}
-    x = Conv2D(128, 1, strides=2, use_bias=False, kernel_initializer='ones')(conv2_2norm)
-    # FIXME Apply following parameters (from Caffe protobuf description):
-    # layer {
-    #   param {lr_mult: 0 decay_mult: 0} # UNARY_BRANCH_PROPAGATE
-    #   convolution_param {
-    #     group: 128
-    #   }
-    # }
+    x = Conv2D_default(128)(x)
+    conv2_2norm = BatchNormalization()(x)
+    x = AveragePooling2D()(x)
 
     # conv3
     # conv3_1
-    x = Conv2D(256, 3, padding='same', activation='relu')(x)
+    x = Conv2D_default(256)(x)
     # conv3_2
-    x = Conv2D(256, 3, padding='same', activation='relu')(x)
+    x = Conv2D_default(256)(x)
     # conv3_3
-    x = Conv2D(256, 3, padding='same', activation='relu')(x)
-    conv3_3_norm = BatchNormalization()(x)  # TODO param {lr_mult: 0 decay_mult: 0}
-    x = Conv2D(256, 1, strides=2, use_bias=False, kernel_initializer='ones')(conv3_3_norm)
-    # FIXME Apply following parameters (from Caffe protobuf description):
-    # layer {
-    #   param {lr_mult: 0 decay_mult: 0} # UNARY_BRANCH_PROPAGATE
-    #   convolution_param {
-    #     group: 256
-    #   }
-    # }
+    x = Conv2D_default(256)(x)
+    conv3_3_norm = BatchNormalization()(x)
+    x = AveragePooling2D()(x)
 
     # conv4
     # conv4_1
-    x = Conv2D(512, 3, padding='same', activation='relu')(x)
+    x = Conv2D_default(512)(x)
     # conv4_2
-    x = Conv2D(512, 3, padding='same', activation='relu')(x)
+    x = Conv2D_default(512)(x)
     # conv4_3
-    x = Conv2D(512, 3, padding='same', activation='relu')(x)
+    x = Conv2D_default(512)(x)
     x = BatchNormalization()(x)
 
     # conv5
     # conv5_1
-    x = Conv2D(512, 3, padding='same', dilation_rate=2, activation='relu')(x)
+    x = Conv2D_default(512, dilation_rate=2)(x)
     # conv5_2
-    x = Conv2D(512, 3, padding='same', dilation_rate=2, activation='relu')(x)
+    x = Conv2D_default(512, dilation_rate=2)(x)
     # conv5_3
-    x = Conv2D(512, 3, padding='same', dilation_rate=2, activation='relu')(x)
+    x = Conv2D_default(512, dilation_rate=2)(x)
     x = BatchNormalization()(x)
 
     # conv6
     # conv6_1
-    x = Conv2D(512, 3, padding='same', dilation_rate=2, activation='relu')(x)
+    x = Conv2D_default(512, dilation_rate=2)(x)
     # conv6_2
-    x = Conv2D(512, 3, padding='same', dilation_rate=2, activation='relu')(x)
+    x = Conv2D_default(512, dilation_rate=2)(x)
     # conv6_3
-    x = Conv2D(512, 3, padding='same', dilation_rate=2, activation='relu')(x)
+    x = Conv2D_default(512, dilation_rate=2)(x)
     x = BatchNormalization()(x)
 
     # conv7
     # conv7_1
-    x = Conv2D(512, 3, padding='same', activation='relu')(x)
+    x = Conv2D_default(512)(x)
     # conv7_2
-    x = Conv2D(512, 3, padding='same', activation='relu')(x)
+    x = Conv2D_default(512)(x)
     # conv7_3
-    x = Conv2D(512, 3, padding='same', activation='relu')(x)
+    x = Conv2D_default(512)(x)
     x = BatchNormalization()(x)
 
     # conv8
     # conv8_1
-    x = Conv2DTranspose(256, 4, padding='same', strides=2, activation='relu')(x)
+    x = Conv2DTranspose(256, 4, padding='same', strides=2)(x)
     # Shortcut
-    # TODO weight_filler { type: "gaussian" std: .01 }
-    # bias_filler { type: "constant" value: 1 }
-    shortcut = Conv2D(256, 3, padding='same')(conv3_3_norm)
+    shortcut = Conv2D(256, 3, padding='same', kernel_initializer=RandomNormal(stddev=.01), bias_initializer='ones')(conv3_3_norm)
     x = Add()([x, shortcut])
     x = Activation('relu')(x)
     # conv8_2
-    x = Conv2D(256, 3, padding='same', activation='relu')(x)
+    x = Conv2D_default(256)(x)
     # conv8_3
-    x = Conv2D(256, 3, padding='same', activation='relu')(x)
+    x = Conv2D_default(256)(x)
     x = BatchNormalization()(x)
 
     # conv9
     # conv9_1
-    # TODO weight_filler { type: "gaussian" std: .01 }
-    # bias_filler { type: "constant" value: 1 }
-    x = Conv2DTranspose(128, 4, padding='same', strides=2, activation='relu')(x)
+    x = Conv2DTranspose(128, 4, padding='same', strides=2, kernel_initializer=RandomNormal(stddev=.01), bias_initializer='ones')(x)
     # Shortcut
-    # TODO weight_filler { type: "gaussian" std: .01 }
-    # bias_filler { type: "constant" value: 1 }
-    shortcut = Conv2D(128, 3, padding='same')(conv2_2norm)
+    shortcut = Conv2D(128, 3, padding='same', kernel_initializer=RandomNormal(stddev=.01), bias_initializer='ones')(conv2_2norm)
     x = Add()([x, shortcut])
     x = Activation('relu')(x)
     # conv9_2
-    # TODO weight_filler { type: "gaussian" std: .01 }
-    # bias_filler { type: "constant" value: 1 }
-    x = Conv2D(128, 3, padding='same', activation='relu')(x)
+    x = Conv2D_default(128, kernel_initializer=RandomNormal(stddev=.01), bias_initializer='ones')(x)
     # conv9_3
-    x = Conv2D(128, 3, padding='same', activation='relu')(x)
+    x = Conv2D_default(128)(x)
     x = BatchNormalization()(x)
 
     # conv10
     # conv10_1
-    # TODO weight_filler { type: "gaussian" std: .01 }
-    # bias_filler { type: "constant" value: 1 }
-    x = Conv2DTranspose(128, 4, padding='same', strides=2, activation='relu')(x)
+    x = Conv2DTranspose(128, 4, padding='same', strides=2, kernel_initializer=RandomNormal(stddev=.01), bias_initializer='ones')(x)
     # Shortcut
-    # TODO weight_filler { type: "gaussian" std: .01 }
-    # bias_filler { type: "constant" value: 1 }
-    shortcut = Conv2D(128, 3, padding='same')(conv1_2norm)
+    shortcut = Conv2D(128, 3, padding='same', kernel_initializer=RandomNormal(stddev=.01), bias_initializer='ones')(conv1_2norm)
     x = Add()([x, shortcut])
     x = Activation('relu')(x)
     # conv10_2
-    # TODO weight_filler { type: "gaussian" std: .01 }
-    # bias_filler { type: "constant" value: 1 }
-    # relu_param { negative_slope: 0.2 }
-    x = Conv2D(128, 3, padding='same', activation='relu')(x)
+    x = Conv2D(128, 3, padding='same', kernel_initializer=RandomNormal(stddev=.01), bias_initializer='ones')(x)
+    x = LeakyReLU(alpha=.2)(x)
     # conv10_ab
     x = Conv2D(2, 1, activation='tanh')(x)
     x = Scale(100)(x)
