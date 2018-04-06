b16a3abd86e09eae658a2b12ee3eef4f2d2c8235
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-04-06 00:10:18 -0300

    Refactor batch generator

--- colormotion/nn/generators.py
+++ colormotion/nn/generators.py
@@ -7,12 +7,12 @@ import colormotion.dataset as dataset
 
 
 class VideoFramesDataGenerator():  # pylint: disable=too-few-public-methods
-    def __init__(self, data_format='channels_last', rescale=1 / 255, contiguous_count=2):
+    def __init__(self, data_format='channels_last', rescale=1 / 255, contiguous_count=1):
         '''Generate groups of contiguous frames from a dataset.
 
         data_format: see keras.preprocessing.image.ImageDataGenerator
         rescale: see keras.preprocessing.image.ImageDataGenerator
-        contiguous_count: number of frames to yield at each call (default 2)
+        contiguous_count: number of previous frames to yield at each call (default 1)
         '''
         # TODO Support data augmentation, similar to https://keras.io/preprocessing/image/
         if data_format != 'channels_last':
@@ -28,16 +28,13 @@ class VideoFramesDataGenerator():  # pylint: disable=too-few-public-methods
                                    target_size=target_size)
 
     def _load_batch(self, start_frames, target_size):
-        state_input = []
-        grayscale_input = []
+        x = [[]] * (self.contiguous_count + 1)
         y = []
         for scene, frame in start_frames:
             sample = self._load_sample(scene, frame, target_size)
-            state_input.append(sample[0])
-            grayscale_input.append(sample[1])
-            y.append(sample[2])
-        print(([state_input, grayscale_input], y))
-        return [np.array(state_input), np.array(grayscale_input)], np.array(y)
+            x = [state + [sample_frame] for sample_frame, state in zip(sample[:-1], x)]
+            y.append(sample[-1])
+        return [np.array(i) for i in x], np.array(y)
 
     def _load_sample(self, scene, start_frame, target_size):
         '''Load a sample to build a batch.'''
@@ -45,22 +42,19 @@ class VideoFramesDataGenerator():  # pylint: disable=too-few-public-methods
             return dataset.read_image(str(dataset.get_frame_path(scene, frame_number)), resolution=target_size)
 
         # y = expected colorization in final frame
-        y = read_image(scene, start_frame + self.contiguous_count - 1)
+        y = read_image(scene, start_frame + self.contiguous_count)
         # network input (previous frames colorized and current frame in grayscale)
         state = [
             self.rescale * read_image(scene, start_frame + i)
-            for i in range(self.contiguous_count - 1)
+            for i in range(self.contiguous_count)
         ]
-        if self.contiguous_count != 2:
-            raise NotImplementedError()
-        state = state[0]  # TODO if self.contiguous_count != 2, should concat all frames
         grayscale = self.rescale * dataset.convert_to_grayscale(y)
         y = self.rescale * y
-        return state, grayscale, y
+        return state + [grayscale, y]
 
     def _get_contiguous_frames(self, frames):
         # Remove frames at the end of a scene and discard too short scenes
         frames = [(directory, frame)
-                  for directory, images in frames.items() if len(images) >= self.contiguous_count
-                  for frame in images[:-self.contiguous_count + 1]]
+                  for directory, images in frames.items() if len(images) > self.contiguous_count
+                  for frame in images[:-self.contiguous_count]]
         return frames
