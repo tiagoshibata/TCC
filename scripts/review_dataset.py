e21ecbe899a4f83d27c1602e11989cd0c288925a
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-05-03 22:53:05 -0700

    Refactor scene merging

--- scripts/review_dataset.py
+++ scripts/review_dataset.py
@@ -36,6 +36,22 @@ def merge_prompt(*directories, viewer='xdg-open'):
             return True
 
 
+def merge_frames(movie_directory, frames):
+    '''Merge a list of frames into the same scene.
+
+    The lowest numbered frame must be at the beginning of a scene.'''
+    frames = sorted(frames)
+    merge_to_scene = dataset.get_scene_directory(movie_directory, frames[0].stem)
+    for frame in frames:
+        if (merge_to_scene / frame.name).exists():
+            continue
+        new_scene = dataset.get_scene_directory(movie_directory, frame.stem, mkdir=False)
+        if new_scene.exists():
+            merge_from_scene = new_scene
+        (merge_from_scene / frame.name).rename(merge_to_scene / frame.name)
+        rmdir_if_empty(merge_from_scene)
+
+
 def merge_short_scenes(movie_directory, scene_frames, viewer):
     # Fast camera movement will generate false negatives in SSIM, which splits the same scene into multiple small
     # scenes. Here, the user will be prompted whether small scenes should be merged.
@@ -52,39 +68,31 @@ def merge_short_scenes(movie_directory, scene_frames, viewer):
                         preview.symlink_to(frame.resolve())
             else:
                 scene = next_scene
-                frames = sorted(temporary_directory.iterdir())
+                frames = temporary_directory.iterdir()
                 if not frames:
                     continue
                 if merge_prompt(temporary_directory, viewer=viewer):
-                    merge_to_scene = dataset.get_scene_directory(movie_directory, frames[0].stem)
-                    for frame in frames:
-                        if (merge_to_scene / frame.name).exists():
-                            continue
-                        new_scene = dataset.get_scene_directory(movie_directory, frame.stem, mkdir=False)
-                        if new_scene.exists():
-                            merge_from_scene = new_scene
-                        (merge_from_scene / frame.name).rename(merge_to_scene / frame.name)
+                    merge_frames(movie_directory, frames)
                 for frame in frames:
                     frame.unlink()
 
 
 def review_dataset(movie_directory, viewer, frames_to_skip=0):
+    scenes = dataset.get_scenes(movie_directory)
     scene_frames = [
         (scene, frames)
-        for scene, frames in dataset.get_scenes(movie_directory).items()
+        for scene, frames in scenes.items()
         if not rmdir_if_empty(scene) and int(scene.name) >= frames_to_skip
     ]
 
     merge_short_scenes(movie_directory, scene_frames, viewer)
 
-    scenes = [x[0] for x in scene_frames]
-    if len(scenes) >= 2:
-        scene = scenes[0]
-        for next_scene in scenes[1:]:
+    scene_ids = list(scenes)
+    if len(scene_ids) >= 2:
+        scene = scene_ids[0]
+        for next_scene in scene_ids[1:]:
             if merge_prompt(scene, next_scene, viewer=viewer):
-                for frame in next_scene.iterdir():
-                    frame.rename(scene / frame.name)
-                next_scene.rmdir()
+                merge_frames(movie_directory, scenes[scene] + scenes[next_scene])
             else:
                 scene = next_scene
 
