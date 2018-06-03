ca74bdec3947b89c71758cf477a169846883a93c
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-06-03 12:03:47 -0700

    Add model checkpoint

--- scripts/train.py
+++ scripts/train.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import random
 import sys
 
+from keras.callbacks import ModelCheckpoint
+
 from colormotion.argparse import directory_path
 from colormotion.nn.generators import VideoFramesDataGenerator
 import colormotion.nn.model as model
@@ -31,12 +33,14 @@ def data_generators(dataset_folder):
 def main(args):
     train_generator, test_generator = data_generators(args.dataset)
     m = model.interactive_colorization(weights_path=args.weights)
+    checkpoint = ModelCheckpoint('epoch-{epoch:03d}-{val_loss:.3f}.hdf5', verbose=1, period=5)
     fit = m.fit_generator(
         train_generator,
         steps_per_epoch=2000,
         epochs=80,
         validation_data=test_generator,
-        validation_steps=10)
+        validation_steps=10,
+        callbacks=[checkpoint])
     print(fit.history)
     m.save('Colorful_model.h5')
     # score = m.evaluate(...)
