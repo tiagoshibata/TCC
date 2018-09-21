1a43b68c415a0ad433cef044d472072d71c1f3e6
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-09-21 15:46:46 -0300

    Simplify data generators in optical flow model

--- scripts/train/filters_optical_flow.py
+++ scripts/train/filters_optical_flow.py
@@ -24,57 +24,62 @@ def parse_args():
     return parser.parse_args()
 
 
-class Generator(VideoFramesGenerator):  # pylint: disable=too-few-public-methods
-    '''Generate groups of contiguous frames from a dataset, with the L*a*b* channels of previous frames as the state.'''
+class Generator(VideoFramesGenerator):
+    '''Generate groups of contiguous frames from a dataset.
+
+    The generated data has inputs [l_input, l_input_tm1, features_tm1, ab_and_mask_input].'''
+    def __init__(self, nn_model, **kwargs):
+        self.model = nn_model
+        super().__init__(**kwargs)
+
+    def load_batch(self, start_frames, target_size):  # pylint: disable=too-many-locals
+        assert self.contiguous_count == 1
+        x_batch = [[], [], [], []]
+        y_batch = []
+        for scene, frame in start_frames:
+            # y = expected colorization in last frame
+            # state = previous frames colorized and current frame in grayscale
+            l, ab = dataset.read_frame_lab(scene, frame + self.contiguous_count, target_size)
+            x_batch[0].append(l)
+            y_batch.append(ab)
+            l_tm1, ab_tm1 = dataset.read_frame_lab(scene, frame, target_size)
+            x_batch[1].append(l_tm1)
+            features_tm1_placeholder = np.empty((32, 32, 512))
+            ab_and_mask_input_placeholder = np.zeros((256, 256, 3))
+            _, features_tm1, _ = self.model.predict(l_tm1, l_tm1,
+                                                    features_tm1_placeholder, ab_and_mask_input_placeholder)
+            x_batch[2].append(features_tm1)
+            # TODO create ab_and_mask_input using ab_tm1 instead of returning a placeholder
+            x_batch[3].append(ab_and_mask_input_placeholder)
+        return [np.array(model_input) for model_input in x_batch], np.array(y_batch)
+
     def load_sample(self, scene, start_frame, target_size):
-        '''Load a sample to build a batch.'''
-        # y = expected colorization in last frame
-        # state = previous frames colorized and current frame in grayscale
-        grayscale, y = dataset.read_frame_lab(scene, start_frame + self.contiguous_count, target_size)
-        state = [
-            np.dstack(dataset.read_frame_lab(scene, start_frame + i, target_size))
-            for i in range(self.contiguous_count)
-        ]
-        return state + [grayscale, y]
+        pass  # unused
 
 
-def data_generators(dataset_folder):
+def data_generators(dataset_folder, nn_model):
     flow_params = {
         'batch_size': 8,
         'target_size': (256, 256),
         'seed': random.randrange(sys.maxsize),
     }
     # TODO Split train and test datasets
-    train = Generator().flow_from_directory(dataset_folder, **flow_params)
-    test = Generator().flow_from_directory(dataset_folder, **flow_params)
+    train = Generator(nn_model).flow_from_directory(dataset_folder, **flow_params)
+    test = Generator(nn_model).flow_from_directory(dataset_folder, **flow_params)
     return train, test
 
 
-def frames_to_model_input(m, generator):
-    '''Convert a pair of frames to the model inputs.'''
-    # The model has inputs [l_input, l_input_tm1, features_tm1, ab_and_mask_input] and
-    # outputs [x, encoded_features, interpolated_features]
-    while True:
-        state, y = next(generator)
-        l_tm1, l = state
-        features_tm1_placeholder = np.empty((32, 32, 512))
-        ab_and_mask_input_placeholder = np.empty((256, 256, 3))
-        _, features_tm1, _ = m.predict(l_tm1, l_tm1, features_tm1_placeholder, ab_and_mask_input_placeholder)
-        yield [l, l_tm1, features_tm1, ab_and_mask_input_placeholder], y
-
-
-
 def main(args):
-    train_generator, test_generator = data_generators(args.dataset)
     m = model()
+    train_generator, test_generator = data_generators(args.dataset, m)
     if args.weights:
         load_weights(m, args.weights)
     checkpoint = ModelCheckpoint('epoch-{epoch:03d}-{val_loss:.3f}.hdf5', verbose=1, period=5)
     fit = m.fit_generator(
-        frames_to_model_input(m, train_generator),
+        train_generator,
         steps_per_epoch=args.steps_per_epoch,
         epochs=args.epochs,
-        validation_data=frames_to_model_input(m, test_generator),
+        validation_data=test_generator,
         validation_steps=args.validation_steps,
         callbacks=[checkpoint])
     print(fit.history)
