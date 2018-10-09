093b917b950b288554c4f608b05d6806c7c60ded
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-10-08 23:57:55 -0300

    Increase validation steps count

--- colormotion/argparse.py
+++ colormotion/argparse.py
@@ -14,7 +14,7 @@ def training_args_parser():
     parser.add_argument('--weights', type=Path, help='weights file')
     parser.add_argument('--steps-per-epoch', type=int, default=2000)
     parser.add_argument('--epochs', type=int, default=80)
-    parser.add_argument('--validation-steps', type=int, default=20)
+    parser.add_argument('--validation-steps', type=int, default=100)
     parser.add_argument('--dry', action='store_true')
     parser.add_argument('dataset', type=directory_path, help='dataset folder')
     return parser
