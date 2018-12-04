5a0c6165d95c4a66f1cd31426e24dac47de91bc9
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-12-04 21:34:14 -0200

    Add --plot flag to summary.py

--- scripts/summary.py
+++ scripts/summary.py
@@ -5,13 +5,17 @@ from keras.models import load_model
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Print a model summary.')
+    parser.add_argument('--plot', action='store_true', help='plot model')
     parser.add_argument('model')
     return parser.parse_args()
 
 
-def summary(path):
-    m = load_model(path)
+def summary(args):
+    m = load_model(args.path)
     m.summary()
+    if args.plot:
+        from keras.utils import plot_model
+        plot_model(m, to_file='model.png')
 
 if __name__ == '__main__':
-    summary(parse_args().model)
+    summary(parse_args())
